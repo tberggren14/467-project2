c29887d2ffac39b70ed5f75e40a88379f21af17f
tberggren14 <89421907+tberggren14@users.noreply.github.com>
2022-04-22 01:58:43 -0500

    Saving for the night

---
+++ public/components/product.js
@@ -0,0 +1,22 @@
+app.component('product', {
+    template:
+
+        `<div class="part">
+         <img :src="part.pictureURL">
+        </div>`,
+
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+
+
+    },
+
+    computed: {
+
+    }
+})
