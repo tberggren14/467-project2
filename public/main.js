bd5f16087ea5f16d707d77b9cf0a9fa54523f05f
tberggren14 <89421907+tberggren14@users.noreply.github.com>
2022-04-24 15:36:08 -0500

    Added main.js to update and delete from cart

---
+++ public/main.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart: [],
+            content: "productList",
+
+        }
+    },
+    methods: {
+        updateTheCart(part) {
+            this.cart.push(part)
+        },
+
+        deleteFromCart(part) {
+            var currentCart = this.cart;
+            this.cart = [];
+            for (let c of currentCart) {
+                if (c !== part) {
+                    this.cart.push(c)
+                }
+            }
+            
+        },
+    }
+})
+
+app.mount('#app')
