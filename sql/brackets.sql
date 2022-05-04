c2abea1cb48cd1b6ada1816fbcaaffb1e88579aa
z1885874 <89593000+z1885874@users.noreply.github.com>
2022-05-04 15:59:50 -0500

    g

--- sql/brackets.sql
+++ sql/brackets.sql
@@ -1,13 +1,13 @@
-/*drop table `brackets`*/
+drop table `brackets`
 
-/*CREATE TABLE `brackets` (
+CREATE TABLE `brackets` 
   `id` int(11) NOT NULL PRIMARY KEY,
   `minweight` DECIMAL(9.2) DEFAULT 0.00,
   `maxweight` DECIMAL(9.2) DEFAULT 0.00,
   `cost` DECIMAL(9.2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;*/
 
-/*INSERT INTO `csci467`.`brackets`
+INSERT INTO `csci467`.`brackets`
 (`id`,
 `minweight`,
 `maxweight`,
@@ -15,7 +15,45 @@
 VALUES
 (1,
 0,
-1,
-1);*/
-
-select * from brackets;
+50,
+30);
+INSERT INTO `csci467`.`brackets`
+(`id`,
+`minweight`,
+`maxweight`,
+`cost`)
+VALUES
+(1,
+50.01,
+100,
+60);
+INSERT INTO `csci467`.`brackets`
+(`id`,
+`minweight`,
+`maxweight`,
+`cost`)
+VALUES
+(1,
+100.01,
+150,
+90);
+INSERT INTO `csci467`.`brackets`
+(`id`,
+`minweight`,
+`maxweight`,
+`cost`)
+VALUES
+(1,
+150.01,
+200,
+120);
+INSERT INTO `csci467`.`brackets`
+(`id`,
+`minweight`,
+`maxweight`,
+`cost`)
+VALUES
+(1,
+200.01,
+999.99,
+300);
