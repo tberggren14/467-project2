444a897262deb23ec2a5bcf66946c58075512b19
Muhammad1212 <70299702+Muhammad1212@users.noreply.github.com>
2022-05-04 19:44:36 -0500

    Update receiving.js

--- views/components/receiving.js
+++ views/components/receiving.js
@@ -1,1 +1,38 @@
+app.component('brackets', {
+    template:
+        <div class="display">
+           
+        </div>,
 
+    data() {
+        return {
+           brackets: null,
+            weight: '',
+            cost: ''
+        }
+    },
+
+    methods: {
+        async getBrackets() {
+            var resp = await axios.get('http://localhost:3000/getBrackets')
+            this.brackets = resp.data
+        }
+
+        async newBrackets() {
+            if (this.weight === '' || this.cost === '') {
+                alert('Inventory items check results')
+                return
+            }
+        }
+
+        }
+    },
+
+    computed: {
+        list() {
+            if (this.parts == null) {
+                this.getBrackets()
+            }
+            return this.brackets
+        }
+    }
