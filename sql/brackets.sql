1b6baae01263324f9d2b21522bf6ed4fac25e070
z1885874 <89593000+z1885874@users.noreply.github.com>
2022-05-04 19:14:57 -0500

    updating to fix Bracket.SQL

--- sql/brackets.sql
+++ sql/brackets.sql
@@ -1,5 +1,59 @@
+drop table brackets;
+
 CREATE TABLE brackets (
-    'id' INT NOT NULL PRIMARY KEY,
-    'weight' DECIMAL (9.2) DEFAULT 0.00,
-    'cost' DECIMAL (9.2) DEFAULT 0.00
-);
+  id int(11) NOT NULL PRIMARY KEY,
+  'minweight' DECIMAL(9.2) DEFAULT 0.00,
+  'maxweight' DECIMAL(9.2) DEFAULT 0.00,
+  'cost' DECIMAL(9.2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO brackets
+(id,
+minweight,
+maxweight,
+cost)
+VALUES
+(1,
+0,
+50,
+30);
+INSERT INTO brackets
+(id,
+minweight,
+maxweight,
+cost)
+VALUES
+(1,
+50.01,
+100,
+60);
+INSERT INTO brackets
+(id,
+minweight,
+maxweight,
+cost)
+VALUES
+(1,
+100.01,
+150,
+90);
+INSERT INTO brackets
+(id,
+minweight,
+maxweight,
+cost)
+VALUES
+(1,
+150.01,
+200,
+120);
+INSERT INTO brackets
+(id,
+minweight,
+maxweight,
+cost)
+VALUES
+(1,
+200.01,
+999.99,
+300);
