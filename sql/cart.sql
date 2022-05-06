c47f08469b1795167ce494794c775ff53ada95c3
tberggren14 <89421907+tberggren14@users.noreply.github.com>
2022-05-06 18:57:06 -0500

    Delete cart.sql

--- sql/cart.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE inCart (
-    'orderid' INT NOT NULL AUTO_INCREMENT,
-    'part_number' INT NOT NULL,
-    'quantity' INT NOT NULL,
-
-    PRIMARY KEY ('orderid','part_number'),
-    FOREIGN KEY ('customerorder') REFERENCES customerorder('orderid')
-);
