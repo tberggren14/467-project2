16f488d34dc71483bbda3a51a0a6ff3bb9603977
Sulejmani97 <s0483973@student.rockvalleycollege.edu>
2022-05-06 15:20:52 -0500

    Order fulfillment page edited

--- controllers/order.js
+++ controllers/order.js
@@ -9,7 +9,7 @@ connection.connect();
 
 module.exports = {
     getAll: async result => {
-        connection.query('SELECT * FROM customerorder', function(err, rows){
+        connection.query('SELECT * FROM customerorder WHERE status = "open"', function(err, rows){
             if (err) throw err;
             result(rows);
         });
